ffe9046b71fa070e383d9f64a3ca437b6f4a60eb
nurbq <nurbakhyt.sembayev@gmail.com>
2024-03-20 22:28:39 +0500

    fix userController

--- src/main/java/lab/andersen/controller/UsersController.java
+++ src/main/java/lab/andersen/controller/UsersController.java
@@ -4,15 +4,14 @@ import com.google.gson.Gson;
 import lab.andersen.dao.UserDao;
 import lab.andersen.dto.UserDto;
 import lab.andersen.entity.User;
-import lab.andersen.exception.UserNotFoundException;
+import lab.andersen.exception.ServiceException;
 import lab.andersen.service.UserService;
 import lab.andersen.servlet.FrontController;
-import lombok.SneakyThrows;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.List;
 
 public class UsersController extends FrontController {
@@ -26,21 +25,16 @@ public class UsersController extends FrontController {
         String pathInfo = request.getPathInfo();
         String[] split = pathInfo.split("/");
         try (PrintWriter writer = response.getWriter()) {
-//            writer.write(pathInfo);
-            writer.write(split[2]);
             if (split.length <= 2) {
                 List<UserDto> all = userService.findAll();
                 writer.write(gson.toJson(all));
             } else {
-                Integer userId = Integer.getInteger(split[2]);
-                writer.write(userId);
-//                if (userId != null) {
-//                    User userById = userService.findById(userId);
-//                    writer.write(gson.toJson(userById));
-//                } else {
-//                    throw new UserNotFoundException("User not found");
-//                }
+                int userId = Integer.parseInt(split[2]);
+                User userById = userService.findById(userId);
+                writer.write(gson.toJson(userById));
             }
+        } catch (RuntimeException | ServiceException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 }
