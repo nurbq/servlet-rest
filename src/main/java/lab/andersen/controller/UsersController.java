06ad2e50d16f3214f3ba2d5f948b35b0fad07a72
nurbq <nurbakhyt.sembayev@gmail.com>
2024-03-20 19:48:40 +0500

    add user controller

--- src/main/java/lab/andersen/controller/UsersController.java
+++ src/main/java/lab/andersen/controller/UsersController.java
@@ -1,17 +1,43 @@
 package lab.andersen.controller;
 
+import com.google.gson.Gson;
+import lab.andersen.dao.UserDao;
+import lab.andersen.dto.UserDto;
+import lab.andersen.entity.User;
+import lab.andersen.exception.UserNotFoundException;
+import lab.andersen.service.UserService;
 import lab.andersen.servlet.FrontController;
+import lombok.SneakyThrows;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class UsersController extends FrontController {
+
+    private final UserService userService = new UserService(new UserDao());
+    private final Gson gson = new Gson();
+
+    @SneakyThrows
     @Override
     public void process() throws ServletException, IOException {
         response.setContentType("application/json");
         try (PrintWriter writer = response.getWriter()) {
-            writer.print(request.getPathInfo());
+            String pathInfo = request.getPathInfo();
+            if (pathInfo == null || pathInfo.equals("/")) {
+                List<UserDto> all = userService.findAll();
+                writer.print(gson.toJson(all));
+            } else {
+                String[] split = pathInfo.split("/");
+                Integer s = Integer.getInteger(split[2]);
+                if (s != null) {
+                    User userById = userService.findById(s);
+                    writer.print(gson.toJson(userById));
+                } else {
+                    throw new UserNotFoundException("User not found");
+                }
+            }
         }
     }
 }
